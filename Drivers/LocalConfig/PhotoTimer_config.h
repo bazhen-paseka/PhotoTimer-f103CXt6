ed1267f4464153ecdc7c5c1b6670259137eeec20
Paseka <bazhen.paseka@gmail.com>
2022-08-26 19:56:46 +0300

    CubeMX creature

---
+++ Drivers/LocalConfig/PhotoTimer_config.h
@@ -0,0 +1,15 @@
+/*
+ * PhotTimer_config.h
+ *
+ *  Created on: 26 серп. 2022 р.
+ *      Author: paseka
+ */
+
+#ifndef LOCALCONFIG_PHOTOTIMER_CONFIG_H_
+#define LOCALCONFIG_PHOTOTIMER_CONFIG_H_
+
+
+	#define SOFT_VERSION 	10
+
+
+#endif /* LOCALCONFIG_PHOTOTIMER_CONFIG_H_ */
